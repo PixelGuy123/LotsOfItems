36534e3c602c8f289b1a8490fd9657a717bf956f
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2025-02-05 23:04:23 -0300

    Update ITM_ChocolateQuarter.cs

--- CustomItems/Eatables/ITM_ChocolateQuarter.cs
+++ CustomItems/Eatables/ITM_ChocolateQuarter.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using PixelInternalAPI.Extensions;
+using UnityEngine;
 
 namespace LotsOfItems.CustomItems.Eatables
 {
 	public class ITM_ChocolateQuarter : ITM_GenericZestyEatable
 	{
+		protected override void VirtualSetupPrefab(ItemObject itemObject)
+		{
+			base.VirtualSetupPrefab(itemObject);
+			audUse = GenericExtensions.FindResourceObjectByName<SoundObject>("CoinDrop");
+		}
 		public override bool Use(PlayerManager pm)
 		{
 			if (Physics.Raycast(pm.transform.position, Singleton<CoreGameManager>.Instance.GetCamera(pm.playerNumber).transform.forward, out hit, pm.pc.reach, pm.pc.ClickLayers))
@@ -11,6 +17,7 @@ namespace LotsOfItems.CustomItems.Eatables
 				IItemAcceptor component = hit.transform.GetComponent<IItemAcceptor>();
 				if (component != null && component.ItemFits(quarterType))
 				{
+					Singleton<CoreGameManager>.Instance.audMan.PlaySingle(audUse);
 					component.InsertItem(pm, pm.ec);
 					Destroy(gameObject);
 					return true;
@@ -24,5 +31,8 @@ namespace LotsOfItems.CustomItems.Eatables
 
 		[SerializeField]
 		internal Items quarterType = Items.Quarter;
+
+		[SerializeField]
+		internal SoundObject audUse;
 	}
 }
